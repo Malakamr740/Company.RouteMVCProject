24d64000b68b3d9ddcd2059cc3baee6f019770b0
Malakamr740 <116198822+Malakamr740@users.noreply.github.com>
2025-09-20 23:47:35 +0300

    Department module Business Logic Layer

---
+++ Company.RouteMVCProject.BusinessLogicLayer/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Company.RouteMVCProject.DataAccessLayer.Models;
+
+namespace Company.RouteMVCProject.BusinessLogicLayer.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        public IEnumerable<Department> GetAll();
+        public Department? Get(int id);
+        public int Add(Department model);
+        public int update(Department model);
+        public int Delete(Department model);
+        
+    }
+}
---
+++ Company.RouteMVCProject.BusinessLogicLayer/Repositories/DepartmentRepository.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Company.RouteMVCProject.BusinessLogicLayer.Interfaces;
+using Company.RouteMVCProject.DataAccessLayer.Data.Contexts;
+using Company.RouteMVCProject.DataAccessLayer.Models;
+
+namespace Company.RouteMVCProject.BusinessLogicLayer.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDBContext context;
+        public IEnumerable<Department> GetAll()
+        {
+           return context.departments.ToList();
+        }
+        public Department? Get(int id)
+        {
+            return context.departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+            context.departments.Add(model);
+            return context.SaveChanges();
+        }
+        public int update(Department model)
+        {
+            context.departments.Update(model);
+            return context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            context.departments.Remove(model);
+            return context.SaveChanges();
+        }
+
+
+    }
+}
